1b128fecc8e0fec1b6067fbb833d89a4ba27ace0
Rob Cobb <rwcobbjr@gmail.com>
2018-02-25 15:31:11 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,21 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data[season].find { |contestant| contestant['status'] == 'Winner' }['name'].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.values.flatten.find { |contestant| contestant['occupation'] == occupation }['name']
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.select { |contestant| contestant['hometown'] == hometown }.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.values.flatten.find { |contestant| contestant['hometown'] == hometown }['occupation']
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data[season].map { |contestant| contestant['age'].to_f }.each_with_index.reduce(0) do |avg, (val, i)|
+    (avg*(i) + val) / (i+1)
+  end.round
 end
